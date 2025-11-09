10ae4287e9dfd970d1527533e0dbd3a9ad100b05
Harsh Jha <116476761+HarshJha2004@users.noreply.github.com>
2025-11-09 07:16:37 +0000

    Conditional Concept Added

---
+++ JS/part3/01_condition-challenges.js
@@ -0,0 +1,54 @@
+// Checking if a number is greater than another number:
+
+let num1 = 5;
+let num2 = 8;
+
+if (num1 > num2) {
+    // console.log(" num 1 is greater than num2");
+} else {
+    // console.log("Nope, num1 is NOT greater");
+}
+
+// console.log("Welcome, To the Real World ");
+
+// Checking if a string is equal to another string:
+
+let username = "HarshJha";
+let anotherUserName = "HarshJha";
+
+if(username == anotherUserName){
+    // console.log("Pick another Username");
+} else {
+    console.log("Username is available");
+}
+
+// Checking if a variable a number or not:
+
+let score = 44;
+
+if (typeof score === "number"){
+    // console.log("Yep, this is number");
+} else {
+    console.log("UMM !, NOT is not a number");
+}
+
+// Check if a boolean value is true or false
+
+    let value = 1;
+
+    if (value == 1) {
+        // console.log(true);
+    } else {
+        console.log(false);
+    }
+
+// Check if array is empty or NOT:
+
+    let items = [];
+    console.log(items.length);
+
+    if (items.length === 0){
+        console.log("Array is Empty");
+    } else {
+        console.log("Array is NOT empty");
+    }
